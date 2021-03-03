8a986d66b61affca5b9f94affc2e19a8903fafa2
PiloudeDakar <64987990+PiloudeDakar@users.noreply.github.com>
2021-03-03 11:38:21 +0100

    Update main.php
    Add the listener

--- src/HiroTeam/BasePlugin/main.php
+++ src/HiroTeam/BasePlugin/main.php
@@ -8,8 +8,12 @@
 namespace HiroTeam\BasePlugin;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
 
-class main extends PluginBase{
-
-   
+class main extends PluginBase implements Listener{
+    //Edit by PiloudeDakar - Sensei#9354
+    public function onEnable()
+    {
+        $this->getSever()->getPluginManager()->registerEvents($this, $this);
+    }
 }
